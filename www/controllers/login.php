3592bcfad3435d2cd36feb6d6fa60e6346db8b5d
geinergv <geiner120300@gmail.com>
2019-07-13 18:52:20 -0500

    DSC

--- www/controllers/login.php
+++ www/controllers/login.php
@@ -2,24 +2,27 @@
 
 # $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}||{$_SERVER['REQUEST_URI']}";
 
+
+# if (strtolower(strval($_SERVER['REQUEST_URI'])) != '/controllers/login.php') {
+if (preg_match("/PRUEBA/i", $_SERVER['REQUEST_URI'])) {
+    header('HTTP/1.0 404 Not Found');
+    readfile('../404.html');
+    exit();
+}
+
 session_start();
 
 echo strtolower(strval($_SERVER['REQUEST_URI']));
-echo '/controllers/login.php';
+echo "\n" . '/controllers/login.php';
+echo "\n" . __FILE__
 
-if (strtolower(strval($_SERVER['REQUEST_URI'])) != '/controllers/login.php') {
 
-    if (!isset($_SESSION["session_id_user"])) {
-        if (!isset($_COOKIE["RSU"]) && !isset($_COOKIE["RCU"])) {
+if (!isset($_SESSION["session_id_user"])) {
+    if (!isset($_COOKIE["RSU"]) && !isset($_COOKIE["RCU"])) {
 
-        } else {
-            include('../models/SignIn.php');
-            
-        }
+    } else {
+        include('../models/SignIn.php');
+
     }
-} else {
-    header('HTTP/1.0 404 Not Found');
-    readfile('../404.html');
 }
-
 ?>
