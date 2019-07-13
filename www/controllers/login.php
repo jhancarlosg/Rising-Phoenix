62de49304a77722cc8ca98bf6905c02bfb36ee51
geinergv <geiner120300@gmail.com>
2019-07-12 22:38:48 -0500

    x

--- www/controllers/login.php
+++ www/controllers/login.php
@@ -1,5 +1,7 @@
 <?php
 
-echo get_headers();
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+
+echo $actual_link;
 
 ?>
