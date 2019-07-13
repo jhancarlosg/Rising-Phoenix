9f184d1a688b4470320df7cabc93df8d6ee56f27
geinergv <geiner120300@gmail.com>
2019-07-13 15:52:08 -0500

    prevenir controllers login

--- www/controllers/login.php
+++ www/controllers/login.php
@@ -2,6 +2,20 @@
 
 # $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}||{$_SERVER['REQUEST_URI']}";
 
+session_start();
 
+if (strtolower(strval($_SERVER['REQUEST_URI'])) != '/controllers/login.php') {
+
+    if (!isset($_SESSION["session_id_user"])) {
+        if (!isset($_COOKIE["RSU"]) && !isset($_COOKIE["RCU"])) {
+
+        } else {
+            include('../models/SignIn.php');
+
+        }
+    }
+} else {
+    header('HTTP/1.0 404 Not Found');
+}
 
 ?>
