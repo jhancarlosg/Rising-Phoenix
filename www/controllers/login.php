df0e4d15844baaa497eb384670734f1baed53c61
geinergv <geiner120300@gmail.com>
2019-07-13 19:04:37 -0500

    #dsctrl

--- www/controllers/login.php
+++ www/controllers/login.php
@@ -4,18 +4,11 @@
 
 
 # if (strtolower(strval($_SERVER['REQUEST_URI'])) != '/controllers/login.php') {
-if (preg_match("/PRUEBA/i", $_SERVER['REQUEST_URI'])) {
-    header('HTTP/1.0 404 Not Found');
-    readfile('../404.html');
-    exit();
-}
+require_once('./Funciones.inc');
+
+notFound('/controllers/login.php');
 
 session_start();
-
-echo strtolower(strval($_SERVER['REQUEST_URI']));
-echo "\n" . '/controllers/login.php';
-echo "\n" . __FILE__;
-
 
 if (!isset($_SESSION["session_id_user"])) {
     if (!isset($_COOKIE["RSU"]) && !isset($_COOKIE["RCU"])) {
