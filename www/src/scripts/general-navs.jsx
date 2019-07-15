04d42c5424b5cb5674e1940f6e3210269531df85
geinergv <geiner120300@gmail.com>
2019-07-15 07:00:07 -0500

    sa

--- www/src/scripts/general-navs.jsx
+++ www/src/scripts/general-navs.jsx
@@ -1,12 +1,13 @@
 function GenNavs(props) {
     const asesores = typeof props.asesores == 'object' ? props.asesores.map((val) => <AsesorOption val={val} key={val} />) : null;
+    console.log(props, asesores)
     return (
         <React.Fragment>
             <p className="navbar-text">{props.correo}</p>
 			<button href="/logout" type="button" className="btn btn-default navbar-btn navbar-right">Cerrar sesión</button>
             <div className="navbar-form navbar-right" role="search">
                 <div className="form-group">
-                <select className="form-control">{props.asesores}</select>
+                <select className="form-control">{asesores}</select>
                 </div>
             </div>
         </React.Fragment>
