5371ce761a4038c0721c677898ab6c5f84eed83b
geinergv <geiner120300@gmail.com>
2019-07-15 13:16:07 -0500

    re

--- www/src/scripts/datos.jsx
+++ www/src/scripts/datos.jsx
@@ -7,7 +7,6 @@ class Datos extends React.Component {
 	componentDidMount() {
 		let xmlhttp = new XMLHttpRequest();
 		let tmp_this = this;
-		let params = `export_data=true`;
 		xmlhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				let data = JSON.parse(this.responseText);
@@ -18,24 +17,27 @@ class Datos extends React.Component {
 		xmlhttp.send();
 	}
 
-	exportarExcel() {
+	exportarExcel(e) {
 		let xmlhttp = new XMLHttpRequest();
+		let params = `export_data=true`;
 		xmlhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
-				let data = JSON.parse(this.responseText);
-				tmp_this.setState({ rows: data});
+				$("#datos-cnt").prepend(Alerta({tipo: "success", msg: "bien"}));
 			}
 		}
 		xmlhttp.open("POST", "/datos", true);
 		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xmlhttp.send();
+		xmlhttp.send(params);
+		e.preventDefault();
 	}
 
 	render () {
 		const rows = this.state.rows.map((row, id) => <Row key={"row"+id} cols={row} />);
 		return (
 			<React.Fragment>
-				<button onClick={this.exportarExcel} type="button" className="btn btn-default btn-lg">EXPORTAR <i className="glyphicon glyphicon-file"></i></button>
+				<form className="form-inline" onSubmit={this.exportarExcel}>
+					<button type="submit" className="btn btn-default btn-lg">EXPORTAR <i className="glyphicon glyphicon-file"></i></button>
+				</form>
 				<table className="table table-hover">
 					<thead>
 						<tr>
